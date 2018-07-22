8b39df3c2edbfb800e70ea65d9bb4a9a4e0cc52d
Antonio Cunanan <antonio.cunanan@gmail.com>
2018-07-22 11:40:48 -0500

    feat: Updated about page.

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {UtilityProvider} from "../../providers/utility/utility.provider";
 
 @IonicPage()
 @Component({
@@ -9,10 +10,15 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
 export class AboutPage {
 
   constructor(public navCtrl: NavController,
-              public navParams: NavParams) {
+              public navParams: NavParams,
+              public utilityProvider: UtilityProvider) {
   }
 
   ionViewDidLoad() {
+  }
+
+  openBrowser(url: string) {
+    this.utilityProvider.openBrowser(url);
   }
 
 }
